0f93d444e274aeeecf1e5a1e12610b552920735b
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-06-06 00:36:20 +0300

    Update TweetsPage.jsx

--- src/pages/TweetsPage/TweetsPage.jsx
+++ src/pages/TweetsPage/TweetsPage.jsx
@@ -1,24 +1,25 @@
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import UsersList from "../../components/UsersList/UsersList";
-import Filter from "../../components/Filter/Filter";
-import Button from "../../shared/components/Button/Button";
-import DotsLdr from "../../shared/components/Loaders/DotsLdr";
+import UsersList from '../../components/UsersList/UsersList';
+import Filter from '../../components/Filter/Filter';
+import Button from '../../shared/components/Button/Button';
+import DotsLdr from '../../shared/components/Loaders/DotsLdr';
 
-import { fetchUsers } from "../../redux/users/users-operations";
+import { fetchUsers } from '../../redux/users/users-operations';
+import { setEmptyItems } from 'redux/users/users-slice';
 import {
   getFilteredUsers,
   getLoading,
-} from "../../redux/users/users-selectors";
+} from '../../redux/users/users-selectors';
 
-import styles from "./TweetsPage.module.scss";
+import styles from './TweetsPage.module.scss';
 
 const TweetsPage = () => {
   const [page, setPage] = useState(1);
   const [isFilterOpen, setIsFilterOpen] = useState(false);
-  const [filter, setFilter] = useState("show all");
+  const [filter, setFilter] = useState('show all');
 
   const dispatch = useDispatch();
 
@@ -30,7 +31,7 @@ const TweetsPage = () => {
   }, [dispatch, page]);
 
   const toggleFilter = () => {
-    setIsFilterOpen((prevState) => !prevState);
+    setIsFilterOpen(prevState => !prevState);
   };
 
   const handleChangeFilter = ({ currentTarget: { value } }) => {
@@ -39,19 +40,23 @@ const TweetsPage = () => {
   };
 
   const loadMore = () => {
-    setPage((prevPage) => prevPage + 1);
+    setPage(prevPage => prevPage + 1);
   };
 
   return (
     <div className={styles.section}>
       <div className={styles.container}>
         <div className={styles.btnWrapper}>
-          <Link to={"/"} className={styles.back}>
+          <Link
+            to={'/'}
+            className={styles.back}
+            onClick={() => dispatch(setEmptyItems([]))}
+          >
             Go back
           </Link>
           <Button onClick={toggleFilter} additionalClassName={styles.adlBtn}>
             Filter
-          </Button>{" "}
+          </Button>{' '}
           {isFilterOpen && (
             <Filter
               filterValue={filter}
